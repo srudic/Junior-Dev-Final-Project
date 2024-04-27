1a2df5d8622b450bb6dc982727209621bcdcf85a
Sanja Rudic <rudic.sanja@gmail.com>
2024-04-27 17:32:21 +0200

    Add custom button

---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ title, icon, titleColor, onClickButton }) => {
+  const handleClick = (e) => {
+    e.stopPropagation(); // Prevent event propagation
+    if (onClickButton) onClickButton();
+  };
+
+  return (
+    <div className={styles.buttonContainer}>
+      <button className={styles.button} onClick={handleClick}>
+        {title && (
+          <span
+            className={styles.buttonSpan}
+            style={titleColor ? { color: titleColor } : {}}
+          >
+            {title}
+          </span>
+        )}
+        <div className={styles.icon}>{icon}</div>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
