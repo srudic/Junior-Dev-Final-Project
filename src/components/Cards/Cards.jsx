87c40f2877a017c658cbab33d479882ba4a8c015
Sanja Rudic <rudic.sanja@gmail.com>
2024-04-27 17:33:26 +0200

    Add delete activity button

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,16 @@
 import styles from "./Cards.module.css";
 import Modal from "../UI/Modal/Modal";
-import { useState } from "react";
+import { useContext, useState } from "react";
+
 import ActivityDetails from "../ActivityDetails/ActivityDetails";
+import Button from "../UI/Button/Button";
+
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+import UserContext from "../../context/UserContext";
 
 const Cards = ({ activities }) => {
+  const { isAdminMode } = useContext(UserContext);
   const [selectedActivity, setSelectedActivity] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -17,6 +24,10 @@ const Cards = ({ activities }) => {
     setIsOpen(false);
   };
 
+  const handleOnClickDelete = () => {
+    console.log("Delete");
+  };
+
   return (
     <div>
       {activities.map((activity, index) => (
@@ -28,6 +39,14 @@ const Cards = ({ activities }) => {
           <h2 className={styles.activityName}>{activity.name}</h2>
           {activity.date && (
             <p className={styles.dateTime}>Datum: {activity.date}</p>
+          )}
+          {isAdminMode && (
+            <Button
+              title="IZBRIŠI"
+              titleColor="#8B0000"
+              icon={<RiDeleteBin5Line size={20} color="#8B0000" />}
+              onClickButton={handleOnClickDelete}
+            />
           )}
         </div>
       ))}
