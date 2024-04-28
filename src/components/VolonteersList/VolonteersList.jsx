c722a7dbcb7bac517448e9c0ccb114d4aef73c63
Sanja Rudic <rudic.sanja@gmail.com>
2024-04-28 14:50:59 +0200

    Add admin's buttons for edit and delete volonteer from the list

--- src/components/VolonteersList/VolonteersList.jsx
+++ src/components/VolonteersList/VolonteersList.jsx
@@ -1,14 +1,19 @@
 import styles from "./VolonteersList.module.css";
 import { LuDot } from "react-icons/lu";
+import { useContext } from "react";
+import UserContext from "../../context/UserContext";
+import Button from "../UI/Button/Button";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { MdEdit } from "react-icons/md";
 
 const Volonteer = ({ name, city, jobs }) => {
+  const { isAdminMode } = useContext(UserContext);
   return (
     <div className={styles.volonteerContainer}>
       <div className={styles.title}>
         <h3 className={styles.name}>{name}</h3>
         <span>{city}</span>
       </div>
-
       {jobs && (
         <div className={styles.jobs}>
           {jobs.map((job) => (
@@ -17,6 +22,12 @@ const Volonteer = ({ name, city, jobs }) => {
               {job.name}
             </p>
           ))}
+        </div>
+      )}
+      {isAdminMode && (
+        <div className={styles.adminButtons}>
+          <Button icon={<MdEdit size={20} color="rgb(29, 143, 29)" />} />
+          <Button icon={<RiDeleteBin5Line size={20} color="#8B0000" />} />
         </div>
       )}
     </div>
