b9f0950903163b6272b4e507482fe9806fdd50ae
Sanja Rudic <rudic.sanja@gmail.com>
2024-04-28 19:21:09 +0200

    Add get request to get associations and approval requests and display fetched data

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,16 +1,57 @@
 import { createContext, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+
+import { db } from "../firebase-config";
 
 const UserContext = createContext({
   isAdminMode: Boolean,
   setIsAdminMode: () => {},
+  associationsList: [],
+  setAssociationsList: () => {},
+  getAssociationsList: () => {},
+  associationsRequestList: [],
+  setAssociationsRequestList: () => {},
+  getAssociationsRequestList: () => {},
 });
 
 export function UserContextProvider({ children }) {
   const [isAdminMode, setIsAdminMode] = useState(false);
+  const [associationsList, setAssociationsList] = useState([]);
+  const [associationsRequestList, setAssociationsRequestList] = useState([]);
+
+  const getAssociationsList = async () => {
+    try {
+      const data = await getDocs(collection(db, "associations"));
+      const responsedData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setAssociationsList(responsedData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getAssociationsRequestList = async () => {
+    try {
+      const data = await getDocs(collection(db, "approval-requests"));
+      const responsedData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setAssociationsRequestList(responsedData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const value = {
     isAdminMode,
     setIsAdminMode,
+    getAssociationsList,
+    associationsList,
+    getAssociationsRequestList,
+    associationsRequestList,
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
--- src/pages/Associations/Associations.jsx
+++ src/pages/Associations/Associations.jsx
@@ -1,41 +1,32 @@
+import { useContext, useEffect } from "react";
+
 import Cards from "../../components/Shared/Cards/Cards";
+import Header from "../../components/Header/Header";
 import Heading from "../../components/UI/Heading/Heading";
+import UserContext from "../../context/UserContext";
 import Wrapper from "../../components/UI/Wrapper/Wrapper";
 
-import { useContext } from "react";
-import UserContext from "../../context/UserContext";
-import Header from "../../components/Header/Header";
-
-const ASSOCIATIONS = [
-  {
-    id: 1,
-    name: "Udruga osoba sa cerebralnom paralizom Srce Split",
-    address: "Mosećka ul. 62",
-    city: "Split",
-  },
-  {
-    id: 2,
-    name: "Udruga MoSt",
-    address: "Ul. Ivana Gundulića 52",
-    city: "Split",
-  },
-  {
-    id: 3,
-    name: "Županijska udruga slijepih Split",
-    address: "Zagrebačka ul. 17",
-    city: "Split",
-  },
-];
-
 const Associations = () => {
-  const { isAdminMode } = useContext(UserContext);
+  const {
+    isAdminMode,
+    getAssociationsList,
+    associationsList,
+    associationsRequestList,
+    getAssociationsRequestList,
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    getAssociationsList();
+    getAssociationsRequestList();
+  }, []);
+
   return (
     <Wrapper>
       <Heading title="Popis udruga" />
       <Header associationsFlag />
-      <Cards data={ASSOCIATIONS} />
+      <Cards data={associationsList} />
       {isAdminMode && <Heading title="Zahtjevi za odobrenje" />}
-      {isAdminMode && <Cards data={ASSOCIATIONS} requestsFlag />}
+      {isAdminMode && <Cards data={associationsRequestList} requestsFlag />}
     </Wrapper>
   );
 };
