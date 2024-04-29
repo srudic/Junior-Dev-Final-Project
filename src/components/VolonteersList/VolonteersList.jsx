cbaf0f1ea6dac5f188fcfc3f20f77fb26c704cd6
Sanja Rudic <rudic.sanja@gmail.com>
2024-04-29 19:28:49 +0200

    Add delete volonteer function

--- src/components/VolonteersList/VolonteersList.jsx
+++ src/components/VolonteersList/VolonteersList.jsx
@@ -1,15 +1,29 @@
-import styles from "./VolonteersList.module.css";
-import { LuDot } from "react-icons/lu";
 import { useContext } from "react";
-import UserContext from "../../context/UserContext";
-import Button from "../UI/Button/Button";
-import { RiDeleteBin5Line } from "react-icons/ri";
-import { MdEdit } from "react-icons/md";
+import { db } from "../../firebase-config";
+import { deleteDoc, doc } from "firebase/firestore";
 import multiavatar from "@multiavatar/multiavatar/esm";
 
-const Volonteer = ({ name, city, jobs }) => {
+import Button from "../UI/Button/Button";
+import UserContext from "../../context/UserContext";
+
+import { LuDot } from "react-icons/lu";
+import { MdEdit } from "react-icons/md";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+import styles from "./VolonteersList.module.css";
+
+const Volonteer = ({ name, city, jobs, id }) => {
   let avatarSVG = multiavatar(name);
-  const { isAdminMode } = useContext(UserContext);
+  const { isAdminMode, getVolonteersList } = useContext(UserContext);
+
+  const deleteVolonteer = async (id) => {
+    try {
+      await deleteDoc(doc(db, "volonteers", id));
+      getVolonteersList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className={styles.volonteerContainer}>
@@ -35,7 +49,10 @@ const Volonteer = ({ name, city, jobs }) => {
       {isAdminMode && (
         <div className={styles.adminButtons}>
           <Button icon={<MdEdit size={20} color="rgb(29, 143, 29)" />} />
-          <Button icon={<RiDeleteBin5Line size={20} color="#8B0000" />} />
+          <Button
+            icon={<RiDeleteBin5Line size={20} color="#8B0000" />}
+            onClickButton={() => deleteVolonteer(id)}
+          />
         </div>
       )}
     </div>
@@ -52,6 +69,7 @@ const VolonteersList = ({ data }) => {
             city={element.city}
             key={element.id}
             jobs={element.activity_types}
+            id={element.id}
           />
         ))}
     </div>
